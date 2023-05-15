39d41114c351b39613ecf52e38d667a703c58e74
Emmy <oemmy0283@gmail.com>
2023-05-15 16:17:03 +0100

    Added my photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import './CTA'
 import CTA from './CTA' //here we import the CTA component inside our header
-import me from '../../assets/logo192.png'
+import me from '../../assets/Oyere.jpg'
 import HeaderSocial from './HeaderSocial' //import of headerSocial component
 export const Header = () => {
 return (
